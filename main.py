07de06dddac2db3db2a1dfff29fbbb49d9a5e406
super02 <othilade@gmail.com>
2022-08-14 19:45:38 +0200

    Actually made reach variable work

--- main.py
+++ main.py
@@ -14,64 +14,9 @@ import requests
 load_dotenv()
 calendarId = os.environ["calendarId"]
 l = Lectio(681)
-reach = 14
+reach = 30 # How many days in the future to check
 alarms = {}
 scheduler = BackgroundScheduler()
-""" 
-def calendarCheck():
-    print("Updating calendar for " + os.environ["user"] + "...")
-    print("Authenticating...")
-    l.authenticate(os.environ["user"], os.environ["pass"])
-    print("Authenticated!")
-    schedule = l.get_schedule_for_student(os.environ["student_id"])
-
-    service = getService()
-
-    for j,day in enumerate(schedule):
-        for i, lesson in enumerate(day):
-            try:
-                _id = "lec"+str(hex(i))[2:]+str(j)+str(lesson.start_time.weekday())+str(lesson.start_time.year)
-            except AttributeError:
-                continue
-            if(lesson.is_cancelled): 
-                try:
-                    if(service.events().get(calendarId=calendarId, eventId=_id).execute()["status"] != "cancelled"):
-                        service.events().delete(calendarId=calendarId, eventId=_id).execute()
-                        print("Deleted " + _id)
-                except:
-                    pass
-                continue
-            event = {
-                'summary': (lesson.subject if lesson.subject != None else lesson.title) + (" | " + lesson.room if lesson.room != None else ""),
-                'location': lesson.room,
-                'description': (lesson.title + "\n" if lesson.title != None else "") + lesson.teacher if lesson.teacher != None else "",
-                'start': {
-                    'dateTime': lesson.start_time.isoformat(),
-                    'timeZone': 'Europe/Copenhagen',
-                },
-                'end': {
-                    'dateTime': lesson.end_time.isoformat(),
-                    'timeZone': 'Europe/Copenhagen',
-                },
-                'id': _id
-            }
-            try:
-                service.events().get(calendarId=calendarId, eventId=_id).execute()
-                service.events().update(calendarId=calendarId, eventId=_id, body=event).execute()
-            except Exception as e:
-                service.events().insert(calendarId=calendarId, body=event).execute()
-            try:
-                if(len(day)-1 == i):
-                    while True:
-                        _id = list(_id)
-                        _id[3]=str(int(_id[3])+1)
-                        _id="".join(_id)
-                        _id = str(hex(_id))[2:]
-                        if(service.events().get(calendarId=calendarId, eventId=_id).execute()["status"] != "cancelled"):
-                            service.events().delete(calendarId=calendarId, eventId=_id).execute()
-                            print("Deleted " + _id)
-            except:
-                pass """
 
 def activateAlarm():
     requests.get("https://api.voicemonkey.io/trigger?access_token=" + os.environ["monkey_access_token"] + "&secret_token=" + os.environ["monkey_secret_token"] + "&monkey=lectiopy")
@@ -135,8 +80,8 @@ def checkday(time: datetime, schedule):
     time_min = time.replace(hour=0, minute=0, second=0, microsecond=0, tzinfo=pytz.timezone("Europe/Copenhagen")).isoformat()
     events = service.events().list(calendarId=calendarId, timeMax=time_max, timeMin=time_min, showDeleted=False).execute()["items"]
     day_schedule = [_lesson for _lesson in schedule if generateTimeID(_lesson.start_time) == generateTimeID(time)]
-    alarm = scheduler.add_job(activateAlarm, 'date', run_date=day_schedule[0].start_time - datetime.timedelta(minutes=75))
-    alarms[time] = alarm # Add alarm to alarms dict
+    alarm = scheduler.add_job(activateAlarm, 'date', run_date=day_schedule[0].start_time - datetime.timedelta(minutes=75)) # Schedule alarm for 75 minutes before first lesson
+    alarms[time_max] = alarm # Add alarm to alarms dict
     for event in events:
         try:
             if(not event["description"].split("\n")[0] in [lesson.url for lesson in day_schedule] and events):
@@ -153,7 +98,7 @@ def updateCalendar():
     l.authenticate(os.environ["user"], os.environ["pass"])
     # Define start and end time as a datetime object
     start = datetime.datetime.now() + timedelta(days=0)
-    end = start + timedelta(days=30)
+    end = start + timedelta(days=reach)
     schedule = l.get_schedule_for_student(os.environ["student_id"], start, end)
     timestamps = []
     for i,lesson in enumerate(schedule):
